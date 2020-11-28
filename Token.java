8b9aaa9d76ce1c55fc1b8b869d860193563782e0
papaps <45527966+papaps@users.noreply.github.com>
2020-11-28 16:56:08 +0800

    Fixed DFA errors

--- Token.java
+++ Token.java
@@ -27,6 +27,8 @@ public class Token{
     private static final int q15 = 15;
     private static final int q16 = 16;
     private static final int q17 = 17;
+    private static final int q18 = 18;
+    private static final int q19 = 19;
     private static final int qdead = -1;
 
     private int state;
@@ -40,80 +42,91 @@ public class Token{
         switch (s) {
             case q0: switch (c) {
                 case 'F': return q1;
-                case '$': return q4;
-                case 'R': return q5;
-                case 'D': return q8;
-                default: return qdead;
-            }
-            case q1: switch (c) {
-                case '1': case '2': case '3': return q2;
-                case '0': case '4': case '5': case '6': case '7': case '8': case '9': return q3;
-                default: return qdead;
-            }
-            case q2: switch (c) {
-                case '0': case '1': return q3;
-                default: return qdead;
-            }
-            case q3: switch (c) {
-                default: return qdead;
-            }
-            case q4: switch (c) {
-                case 'F': return q1;
-                case '1': case '2': case '3': return q6;
-                case '0': case '4': case '5': case '6': case '7': case '8': case '9': return q7;
-                default: return qdead;
-            }
-            case q5: switch (c) {
-                case '1': case '2': case '3': return q6;
-                case '0': case '4': case '5': case '6': case '7': case '8': case '9': return q7;
-                default: return qdead;
-            }
-            case q6: switch (c) {
-                case '0': case '1': return q7;
-                default: return qdead;
-            }
-            case q7: switch (c) {
-                default: return qdead;
-            }
-            case q8: switch (c) {
-                case 'A': return q9;
-                case 'M': return q13;
-                default: return qdead;
-            }
-            case q9: switch (c) {
+                case '$': return q5;
+                case 'R': return q6;
                 case 'D': return q10;
                 default: return qdead;
             }
+            case q1: switch (c) {
+                case '1': case '2': return q3;
+                case '3': return q2;
+                case '0': case '4': case '5': case '6': case '7': case '8': case '9': return q4;
+                default: return qdead;
+            }
+            case q2: switch (c) {
+                case '0': case '1': return q4;
+                default: return qdead;
+            }
+            case q3: switch (c) {
+                case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': return q4;
+                default: return qdead;
+            }
+            case q4: switch (c) {
+                default: return qdead;
+            }
+            case q5: switch (c) {
+                case 'F': return q1;
+                case '0': case '4': case '5': case '6': case '7': case '8': case '9': return q9;
+                case '1': case '2': return q7;
+                case '3': return q8;
+                default: return qdead;
+            }
+            case q6: switch (c) {
+                case '0': case '4': case '5': case '6': case '7': case '8': case '9': return q9;
+                case '1': case '2': return q7;
+                case '3': return q8;
+                default: return qdead;
+            }
+            case q7: switch (c) {
+                case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': return q9;
+                default: return qdead;
+            }
+            case q8: switch (c) {
+                case '0': case '1': return q9;
+                default: return qdead;
+            }
+            case q9: switch (c) {
+                default: return qdead;
+            }
             case q10: switch (c) {
-                case 'D': return q11;
+                case 'A': return q11;
+                case 'M': return q15;
                 default: return qdead;
             }
             case q11: switch (c) {
-                case 'I': return q12;
-                case 'U': return q17;
+                case 'D': return q12;
                 default: return qdead;
             }
             case q12: switch (c) {
-                case 'U': return q17;
+                case 'D': return q13;
                 default: return qdead;
             }
             case q13: switch (c) {
-                case 'U': return q14;
+                case 'I': return q14;
+                case 'U': return q19;
                 default: return qdead;
             }
             case q14: switch (c) {
-                case 'L': return q15;
+                case 'U': return q19;
                 default: return qdead;
             }
             case q15: switch (c) {
-                case 'T': return q16;
+                case 'U': return q16;
                 default: return qdead;
             }
             case q16: switch (c) {
-                case 'U': return q17;
+                case 'L': return q17;
                 default: return qdead;
             }
             case q17: switch (c) {
+                case 'T': return q18;
+                default: return qdead;
+            }
+            case q18: switch (c) {
+                case 'U': return q19;
+                default: return qdead;
+            }
+            case q19: switch (c) {
                 default: return qdead;
             }
             default: return qdead;
@@ -125,11 +138,11 @@ public class Token{
             char c = word.charAt(i);
             state = transitions(state, c);
         }
-        if (state == 2 || state == 3)
+        if (state == 2 || state == 3 || state == 4)
             return Token.TokenType.FPR;
-        else if (state == 6 || state == 7)
+        else if (state == 7 || state == 8 || state == 9)
             return Token.TokenType.GPR;
-        else if (state == 16 || state == 17)
+        else if (state == 18 || state == 19)
             return Token.TokenType.KEYWORD;
         else
             return Token.TokenType.ERROR;
